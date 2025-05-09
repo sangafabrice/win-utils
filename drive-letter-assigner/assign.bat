06cab1418820ccd8c77b2f94f54d3bc4a47c192e
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2025-05-10 00:12:46 +0100

    feat: Remove graceful error handling

--- drive-letter-assigner/assign.bat
+++ drive-letter-assigner/assign.bat
@@ -1,11 +1,8 @@
 @echo off
 
 :main <%1 = disk filter argument> <%2 = drive letter> <%3 = disk partition number [= 1]>
-:: Assign the drive letter to the disk partition.
-:: the script log file path: %temp%\dp-assign-drive-letter.log
 
 setlocal EnableExtensions EnableDelayedExpansion
-:: Script GUID to easily identify the script runner process
 set guid=/guid:5d972861-1b53-49ea-8d6c-5debe634f8d2& set commandName=%~n0& set commandPath=%~f0& set commandParent=%~dp0& set powershell=powershell -NoProfile -ExecutionPolicy ByPass -File& set lib=!commandParent!\lib& set tempRoot=%temp%\dp-assign-drive-letter& set tempRoot=!tempRoot:\\=\!& set defaultPartitionIndexArg=1&
 :startParsing
 call :dequoteArgument %*
@@ -20,7 +17,6 @@ call :isPartitionSpecifierLetter "%returnedArg%" || if not errorlevel 0 ( for /f
 if "%argCount%"=="3" call :echoError 201 %~3 || goto :farEnd
 :volumeParsing
 for /f "tokens=2 delims=:" %%l in ("%returnedArg%") do for /f %%L in ('%powershell% "%lib%\convertTo_upperDriveLetter.ps1" %%~l') do (
-	call :isPartitionNot_C_Drive %%~L || goto :farEnd
 	call :getPartitionName %%~L assigneeDiskIndex assigneePartitionIndex assigneePartitionName || call :echoError 209 %%~L || goto :farEnd
 	set "assigneeCurrentLetter=%%~L"
 )
@@ -36,16 +32,12 @@ call :setNonPositionedArgs %driverLetterArg% %partitionIndexArg% driveLetter ass
 if errorlevel 0 ( set "assigneeDiskIndex=%errorlevel%" ) else ( if not errorlevel -999 ( call :echoError 203 %assigneePartitionIndex% %errorlevel:-999=% ) else if %errorlevel%==-999 call :echoError 204 ) || goto :farEnd
 set assigneePartitionName=Disk #%assigneeDiskIndex%, Partition #%assigneePartitionIndex%
 call :getDriveLetter %assigneeDiskIndex% %assigneePartitionIndex% assigneeCurrentLetter || if errorlevel 1 ( goto :getComputedInfo ) else goto :farEnd
-call :isPartitionNot_C_Drive %assigneeCurrentLetter% || goto :farEnd
 :getComputedInfo
-call :isPartitionNot_C_Drive %driveLetter% || goto :farEnd
-if "%driveLetter%"=="%assigneeCurrentLetter%" goto :end
 set assigneePartitionName=Disk #%assigneeDiskIndex%, Partition #%assigneePartitionIndex%& set assignorNextLetter=%assigneeCurrentLetter%&
 call :echoTitle %driveLetter% "%assigneePartitionName%"
 if defined assigneeCurrentLetter call :echoNeutralWarning 106 "%assigneePartitionName%" %assigneeCurrentLetter%
 :command
 echo %driveLetter%:_%assigneeCurrentLetter%:| find /i "%~d0" > nul && xcopy "%commandParent%" "%tempRoot%" /i /y /s > nul 2>&1 && cd /d "%tempRoot%" 2> nul && set "commandPath=%tempRoot%\%~nx0"
-:: Restart the runner process with the guid argument
 cmd /d /c "%commandPath%" %guid%
 goto :farEnd
 :afterCommand
@@ -111,8 +103,6 @@ set message[204]=ERROR: The disk device was not found.& goto :_echoMessage
 set message[205]=ABORT: The %~2's owner did not release it.& goto :_echoMessage
 :_getMessage[206] <%2 = drive letter>
 set message[206]=ABORT: The letter %~2 was not assigned to the target partition.& goto :_echoMessage
-:_getMessage[207]
-set message[207]=ABORT: Modifying the C partition is not allowed.& goto :_echoMessage
 :_getMessage[208] <%2 = assignee current letter> <%3 = assignee partition name>
 set message[208]=ERROR: Failed to reassign %~2 to "%~3".& goto :_echoMessage
 :_getMessage[209] <%2 = assignee current letter>
@@ -127,7 +117,6 @@ echo %~0| findstr /e "Error" > nul && exit /b -999
 exit /b 0
 
 :echoNeutralWarning <%* = warning message text>
-:: echo warning without changing the errorlevel value
 setlocal
 set errLvl=%errorlevel%
 call :echoWarning %*
@@ -143,9 +132,6 @@ echo.
 exit /b 0
 
 :getDiskFilterClause <%1 = unquoted disk filter argument varialbe name> <%2 = [out] disk filter criteria>
-:: the disk number (i) argument is converted to: Index=i
-:: the disk model substring (s) argument is converted to: FriendlyName like %s%
-:: the disk serial number (sn) argument is converted to: SerialNumber='sn'
 echo !%~1!| findstr /ixrc:"/i:[0-9][0-9]*" | findstr /ixrvc:"/i:0[0-9][0-9]*" > nul && set %~2=!%~1:/i:=-Number !&& exit /b 0
 echo !%~1!| findstr /ibrc:"/m:" > nul && set %~2=!%~1:/m:=-FriendlyName '*!*'&& exit /b 0
 echo !%~1!| findstr /ibrc:"/s:" > nul && set %~2=!%~1:/s:=-SerialNumber '!'&& exit /b 0
@@ -166,10 +152,6 @@ exit /b 999
 call :log ABORT: The task stopped.
 %powershell% "%lib%\get_help.ps1" %commandName%
 exit /b 1
-
-:isPartitionNot_C_Drive <%1 = partition drive letter>
-echo %~1| find /i /v "C" > nul || call :echoError 207
-exit /b
 
 :isPartitionSpecifierLetter <%1 = partition specifier option>
 echo %~1| findstr /ixrc:"/v:[A-Z]" > nul && exit /b 0
